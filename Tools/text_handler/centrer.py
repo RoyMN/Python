925f68ecfc12601448308893924603c2f4e96e43
Roy Morten Nordbø <roy.nordbo@student.uib.no>
2019-05-22 14:04:31 +0200

    line 1, misplaced quotation-marks.

--- Tools/text_handler/centrer.py
+++ Tools/text_handler/centrer.py
@@ -1,4 +1,4 @@
-def center_line("line, length=80"):
+def center_line(line, length=80):
     """Returns a centered copy of a line. The length-specifier decides
     the length of the full line. Remaining space on full line from
     the inputted line will be filled with spaces."""
