50b558e0adf57bee292213dd4d7f3dccbce16681
Roy Morten Nordbø <roy.nordbo@student.uib.no>
2019-11-05 13:27:13 +0100

    Cleanup

--- algorithms/Div/pascal.py
+++ algorithms/Div/pascal.py
@@ -1,4 +1,5 @@
-def pascal(n, base=1):
+def pascal_tree(n, base=1):
+
     """Makes a dictionary where the keys are row-indexes in a pascal-trangle
     of size n, and the values are the rows as a list. E.g. pascal(3) should
     return {1 : [1], 2: [1,1], 3: [1,2,1]}.
@@ -7,18 +8,17 @@ def pascal(n, base=1):
     
     Optional argument 'base=': set an integer as a new base. E.g.
     pascal(3, base=9) should return {1: [2], 2: [2, 2], 3: [2, 4, 2]}"""
+
     
-    #  Conditions in which the function makes sense.
-    assert isinstance(n, (int)), 'n must be an integer. n: {0}'.format(n)
-    assert isinstance(base, (int)), 'base must be an integer. base: {0}'.format(base)
-    
+    for v in (n,base):
+        assert isinstance(v, int),f'Both n and base must be integers'
     if not n:
         return {}
     if n == 1:
-        return {1: [base]} #  Base case.
+        return {1: [base]}
     else:
         bottom_row = list()
-        prev_p = pascal(n-1, base) #  Only do one recursive call!
+        prev_p = pascal_tree(n-1, base)  #Only one recursive call
         for i in range(0, n):
             if i == 0:
                 bottom_row.append(prev_p[n-1][i])
@@ -33,4 +33,4 @@ def pascal(n, base=1):
         
 if __name__ == '__main__':
 
-    print(pascal(7))
+    print(pascal_tree(7))
